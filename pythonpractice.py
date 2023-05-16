d08cc37609a705273debf7be58d5652e5a42c538
champurad0 <ginoduarte17@gmail.com>
2023-05-15 19:57:33 -0700

    updates

--- pythonpractice.py
+++ pythonpractice.py
@@ -1,4 +1,14 @@
-#create a program to tell us how many days, weeks, and months we have left until we are 90 years old
-#create inputs for current age 
-#90 yrs = 1080.74 months | 4696.07 | 32872.50
+#create a tip calculator 
 
+total_bill = float(input("What was the total of the bill? \n"))
+
+percent_tip = float(input("What percentage of tip would you like to leave? \n"))
+tip = percent_tip / 100
+tip_total = tip * total_bill
+meal_total = total_bill + tip_total
+
+total_people = int(input("How many people will split the bill? \n"))
+
+total_per_person = meal_total / total_people
+ 
+print(f"Each person will have to pay {round(total_per_person, 2)}")
