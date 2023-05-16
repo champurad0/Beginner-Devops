b093f9163e5d8a626219a3236babd7d5320ff705
champurad0 <ginoduarte17@gmail.com>
2023-05-15 17:44:59 -0700

    python exercise 3

---
+++ pythonpractice.py
@@ -0,0 +1,4 @@
+#create a program to tell us how many days, weeks, and months we have left until we are 90 years old
+#create inputs for current age 
+#90 yrs = 1080.74 months | 4696.07 | 32872.50
+
